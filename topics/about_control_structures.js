d1045d360b45157d6507b3b55f38142613065bb6
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-07-02 19:19:47 +0100

    Testing switch statement

--- topics/about_control_structures.js
+++ topics/about_control_structures.js
@@ -48,7 +48,7 @@ test("switch", function() {
 			result = 2;
 			break;
 	}
-	equal(__, result, 'what is the value of result?');
+	equal(2, result, 'what is the value of result?');
 });
 
 test("switch default case", function() {
